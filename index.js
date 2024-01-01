3deedd06fb141ebb463682a576c40b29a14888c5
Rinad Abu Qauod <rinadaq98@gmail.com>
2024-01-01 20:21:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+// helpers.js
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  function wrapAdjective(highlight = "*") {
+    return function(adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  }
+  
+ 
+  
